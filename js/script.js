2a2555f3ddf5d29dc05b30d7f9acdad1c45825ef
Richard Mora <llody777@gmail.com>
2026-04-09 00:11:39 -0700

    added a background and event listener that change colors on hover to the social media icons

--- js/script.js
+++ js/script.js
@@ -1,42 +1,14 @@
-// Code from W3Schools
+const icons = document.querySelectorAll(".icons");
 
-// For hamburger menu
+icons.forEach((icon) => {
 
-// create a variable that points to a tag with a class named nav-button on our HTML
-const hamburger = document.querySelector("#nav-button");
+  icon.addEventListener("mouseover", (e) => {
+    e.target.style.background = "#F5E616";
+  });
+  
+  icon.addEventListener("mouseout", (e) => {
+    e.target.style.background = "#dadfe7";
+  });
 
-const navMenu = document.querySelector(".nav-menu");
-
-// This will look for an instance where our hamburger button is clicked
-
-// This will look for an instance where our button is clicked
-
-hamburger.addEventListener("click", () => {
-  const isOpened = hamburger.getAttribute("aria-expanded");
-
-  const visibility = navMenu.getAttribute("data-visible");
-
-  // for our navigation button that makes it goes from open to closed
-
-  if (isOpened === "false") {
-    hamburger.setAttribute("aria-expanded", "true");
-  } else if (isOpened === "true") {
-    hamburger.setAttribute("aria-expanded", "false");
-  }
-
-  // for our navigation menu
-
-  if (visibility === "false") {
-    navMenu.setAttribute("data-visible", "true");
-  } else if (visibility === "true") {
-    navMenu.setAttribute("data-visible", "false");
-  }
 });
 
-// To close nav menu when clicking on one of the links
-document.querySelectorAll(".nav-link").forEach((n) =>
-  n.addEventListener("click", () => {
-    hamburger.setAttribute("aria-expanded", "false");
-    navMenu.setAttribute("data-visible", "false");
-  })
-);
